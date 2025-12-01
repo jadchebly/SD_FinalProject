3f886ac086d534fab5eec7f3d9bd916768970e9a
manuelfeliciano <christianmfeliciano2022@gmail.com>
2025-12-01 18:24:21 +0100

    Added installations for initial step

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import { Routes, Route, Link } from 'react-router-dom'
+import './style.css'
+
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Welcome to your React Router app!</p>
+    </div>
+  )
+}
+
+function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is the about page.</p>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        {/*sign in/sign up page at start*/}
+        <Route path="/" element={<Home />} />
+        {/*dashboard page*/}
+        <Route path="/dashboard" element={<About />} />
+        {/*create post page*/}
+        <Route path="/create-post" element={<About />} />
+        {/*profile page*/}
+        <Route path="/profile" element={<About />} />
+        {/*sign up page*/}
+        <Route path="/signup" element={<About />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
+
