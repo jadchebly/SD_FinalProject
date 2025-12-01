6f592a30b4ade077db21b0b693de2668ecc710cc
manuelfeliciano <christianmfeliciano2022@gmail.com>
2025-12-01 20:20:07 +0100

    Adding Navbar to appropriate places

---
+++ src/components/Dashboard/Dashboard.tsx
@@ -0,0 +1,14 @@
+import Navbar from "./Navbar/Navbar";
+import "./Dashboard.css";
+
+export default function Dashboard() {
+    return (
+      <div className="dashboard-container">
+        <Navbar />
+        <div className="dashboard-content">
+          <h1>Dashboard</h1>
+          <p>This is the dashboard page.</p>
+        </div>
+      </div>
+    );
+  }
---
+++ src/components/Dashboard/Navbar/Navbar.tsx
@@ -0,0 +1,59 @@
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { AiFillPlusCircle } from "react-icons/ai";
+import { IoHome } from "react-icons/io5";
+import { IoArrowBack } from "react-icons/io5";
+import { GiEgyptianProfile } from "react-icons/gi";
+import "./Navbar.css"; 
+
+export default function Navbar() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const isCreatePost = location.pathname === "/create-post";
+
+  const handleBack = () => {
+    navigate(-1);
+  };
+
+  return (
+    <div>
+      <div className="top-navbar">
+        {isCreatePost ? (
+          <button onClick={handleBack} className="back-button">
+            <IoArrowBack size={24} />
+          </button>
+        ) : (
+          <div className="top-navbar-logo">IEstagram</div>
+        )}
+        
+        {!isCreatePost && (
+          <div className="search-container">
+            <input type="text" className="form-control search-input" placeholder="Search" />
+          </div>
+        )}
+        
+        <div className="top-navbar-links">
+          {!isCreatePost && (
+            <>
+              <Link className="link" to="/dashboard">
+                <IoHome size={24} />
+              </Link>
+              <Link className="link" to="/create-post">
+                <AiFillPlusCircle className="text-blue-500" size={24} />
+              </Link>
+            </>
+          )}
+          <div
+            tabIndex={0}
+            role="button"
+            className="avatar"
+          >
+            <Link className="link" to="/profile">
+              <GiEgyptianProfile size={24} />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>  
+  );
+}
+
