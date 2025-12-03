f13db4d221e0ba16e4563201a9fe54cf7d236c0e
Sebastian Feliciano <feliciano.sebastian2004@gmail.com>
2025-12-03 03:36:19 +0100

    deleting mds

--- backend/check_schema.sql
+++
@@ -1,7 +0,0 @@
--- Check if there are any constraints on the posts table
-SELECT 
-    conname AS constraint_name,
-    contype AS constraint_type,
-    pg_get_constraintdef(oid) AS constraint_definition
-FROM pg_constraint
-WHERE conrelid = 'posts'::regclass;
